450deaa94aee0370609bf7d8638451d3a4ea298c
kadiatwinkle123 <156327407+kadiatwinkle123@users.noreply.github.com>
2024-11-14 10:42:23 +0530

    Added whole project
    Added whole project

---
+++ Spring-boot-crud-tutorial-main/src/main/java/com/twinkle/crud/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.twinkle.crud.repository;
+
+import com.twinkle.crud.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+}
---
+++ Spring-boot-crud-tutorial-main/src/main/java/com/twinkle/crud/service/EmployeeService.java
@@ -0,0 +1,18 @@
+package com.twinkle.crud.service;
+
+import com.twinkle.crud.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    Employee saveEmployee(Employee employee);
+
+    List<Employee> fetchAllEmployees();
+
+    Employee getEmployeeById(Long id);
+
+    Employee updateEmployeeById(Long id, Employee employee);
+
+    String deleteEmployeeById(Long id);
+}

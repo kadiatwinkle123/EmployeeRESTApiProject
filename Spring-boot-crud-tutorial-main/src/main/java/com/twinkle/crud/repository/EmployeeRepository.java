ab31107c486db1cd33358295d45a1fc78ef8dec1
kadiatwinkle123 <156327407+kadiatwinkle123@users.noreply.github.com>
2024-11-22 20:45:37 +0530

    Update EmployeeRepository.java
    rename twinkle to venus

--- Spring-boot-crud-tutorial-main/src/main/java/com/twinkle/crud/repository/EmployeeRepository.java
+++ Spring-boot-crud-tutorial-main/src/main/java/com/twinkle/crud/repository/EmployeeRepository.java
@@ -1,6 +1,6 @@
 package com.venus.crud.repository;
 
-import com.twinkle.crud.entity.Employee;
+import com.venus.crud.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
